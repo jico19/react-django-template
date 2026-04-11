db3b7cc582d2898d02ec6d3bffc0339bbee28bb4
jerwin_nico <137849267+jerwinq19@users.noreply.github.com>
2026-04-11 21:02:32 +0800

    added tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
-import { Route, Routes } from "react-router-dom"
-import Homepage from "./pages/Homepage"
+
 
 
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Homepage />}/>
-    </Routes>
+    <div>
+      <h1>Django x React Template</h1>
+    </div>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BrowserRouter } from 'react-router-dom'
 
 
 const queryClient = new QueryClient()
@@ -11,9 +10,7 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </QueryClientProvider>
   </StrictMode>,
 )
