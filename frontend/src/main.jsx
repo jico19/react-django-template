721e640b20ec00cd11c53efa3e32ffd63f98ff81
jerwin_nico <137849267+jerwinq19@users.noreply.github.com>
2026-04-11 21:09:06 +0800

    added a sonner

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from 'sonner'
 
 
 const queryClient = new QueryClient()
@@ -10,6 +11,7 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+      <Toaster />
       <App />
     </QueryClientProvider>
   </StrictMode>,
